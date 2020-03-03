0f9c2d77bb26a2426c37f81018f98d7178c4cab8
Eli Finkel <Eli.Finkel@checkmarx.com>
2020-03-03 17:07:57 +0200

    added jvm config

--- agent.cpp
+++ agent.cpp
@@ -1,1 +1,8 @@
 #include "shared.h"
+
+extern "C" JNIEXPORT jint JNICALL
+Agent_OnLoad(JavaVM* vm, char* options, void* reserved)
+{
+	printf("Hi, Agent_OnLoad\n");
+	return 0;
+}
--- shared.h
+++ shared.h
@@ -1,7 +1,18 @@
 #ifndef SHARED_H
 #define SHARED_H
 
-//??#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-//??#include <windows.h>
+#include <jni.h>
+#pragma comment(lib, "jvm")
+
+// define export
+#ifdef _WIN32
+#ifdef JAVANATIVEAGENTTEST_EXPORTS
+#define API_EXPORT __declspec(dllexport)
+#else
+#define API_EXPORT
+#endif // JAVANATIVEAGENTTEST_EXPORTS
+#else
+#define API_EXPORT
+#endif // _WIN32
 
 #endif //SHARED_H
